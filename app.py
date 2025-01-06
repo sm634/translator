11e4b6e88dedea1f463e81930e8baccbd831b193
Safal Mukhia <safal.mukhia@ibm.com>
2025-01-06 11:12:59 +0000

    initial commit
    Signed-off-by: Safal Mukhia <safal.mukhia@ibm.com>

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
