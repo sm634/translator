e905051c0d26bc3657f66bbdaff6b82b4cdb4cd9
Safal Mukhia <safal.mukhia@ibm.com>
2025-01-07 14:11:40 +0000

    new html and css scripts
    Signed-off-by: Safal Mukhia <safal.mukhia@ibm.com>

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template, session
+import requests, os, uuid, json
 from dotenv import load_dotenv
 
 
@@ -11,3 +12,53 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/soul', methods=['GET'])
+def soul_pic():
+    return render_template('simple_html.html')
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    # Load the values from .env
+    key = os.environ['AZURE_TRANSLATOR_KEY']
+    endpoint = os.environ['AZURE_TEXT_TRANSLATION_ENDPOINT']
+    location = os.environ['AZURE_REGION']
+
+    # Indicate taht twe want to translate and the API version (3.0) and the target langauge
+    path = '/translate?api-version=3.0'
+    # Add the target langauge parameter
+    target_language_parameter = '&to=' + target_language
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Set up the header information, which includes our subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
+
+    # Make the call using post
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    # Call render template, passing the translated text,
+    # original text, and target language to the template
+    return render_template(
+        'results.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
